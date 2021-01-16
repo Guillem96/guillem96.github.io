469c9170aea42770dbd46723dca893881d0ef9ec
Guillem96 <guillem.orellana@gmail.com>
2021-01-16 09:48:19 +0100

    Angular update

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, forkJoin } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
@@ -8,18 +8,38 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class GithubService {
-  private BASE_URL = "https://api.github.com";
-  private AUTH_HEADER = {"Authorization": `token ${environment.githubToken}`};
-  private REPOS_IDS = [
-    'argon-nx', 'efficientdet-tf', 'zsl-pytorch', 'spynet-pytorch', 
-    'rl-examples-jax', 'pointer-nn-pytorch', 'jax-hidden-markov-model',
-    'tensorflow-challenge'
-  ];
+  private BASE_URL = "https://api.github.com/graphql";
+  private AUTH_HEADER = {Authorization: `token ${environment.githubToken}`};
+
   constructor(private http: HttpClient) { }
 
   public getRepositories(): Observable<any> {
-    const urls = this.REPOS_IDS.map(o => `${this.BASE_URL}/repos/Guillem96/${o}`);
-    const requests = urls.map(o => this.http.get(o));
-    return forkJoin(requests);
+    const header = {headers: new HttpHeaders(this.AUTH_HEADER)};
+
+    const query = `
+      query {
+        user(login:"Guillem96") {
+          pinnedItems(first: 10, types: [REPOSITORY, GIST]) {
+            totalCount
+            edges {
+              node { 
+                ... on Repository {
+                  name
+                  description
+                  stargazerCount
+                  forkCount
+                  primaryLanguage {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }`;
+
+    return this.http.post(this.BASE_URL, {query}, header).pipe(
+      map(res => res['data']['user']['pinnedItems']['edges'].map(o => o.node))
+    );
   }
 }
--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -13,6 +13,7 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit(): void {
     this.github.getRepositories().subscribe(res => {
+      console.log(res);
       this.repositories = res;
       this.repositories = this.repositories
         .sort((a, b) => b.stargazers_count - a.stargazers_count);
